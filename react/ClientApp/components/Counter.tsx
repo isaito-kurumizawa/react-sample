50ee3fa204e06798cd09d29e822a091203effac6
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-06-08 15:44:11 +0900

    Add files via upload

--- react/ClientApp/components/Counter.tsx
+++ react/ClientApp/components/Counter.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
 
-interface CounterState {
+interface ICounterState {
     currentCount: number;
 }
-
+// eeact.Component<{},any>の<{},any>が重要。boot.tsxで苦労する
 export class Counter extends React.Component<{},any> {
     constructor() {
+        // super キーワードは、オブジェクトの親の関数を呼び出すために使用できます。
         super();
         this.state = { currentCount: 0 };
     }
-    public test: string = "test";
     render(): JSX.Element {
-        return <div>
-            <h1>Counter</h1>
+        return 　<div>
+                    <h1>Counter</h1>
 
-            <p>This is a simple example of a React component.</p>
-            <p>Current count: <strong>{ this.state.currentCount }</strong></p>
+                    <p>This is a simple example of a React component.</p>
+                    <p>Current count: <strong>{ this.state.currentCount }</strong></p>
 
-            <button onClick={ () => { this.incrementCounter(); } }>Increment</button>
-        </div>;
+                    <button onClick={ () => { this.incrementCounter(); } }>Increment</button>
+                </div>;
     }
 
     incrementCounter(): void {
--- react/ClientApp/components/FetchData.tsx
+++ react/ClientApp/components/FetchData.tsx
@@ -1,61 +1,61 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
-import 'isomorphic-fetch';
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+import "isomorphic-fetch";
 
-interface FetchDataExampleState {
-    forecasts: WeatherForecast[];
+interface IFetchDataExampleState {
+    forecasts: IWeatherForecast[];
     loading: boolean;
 }
 
-export class FetchData extends React.Component<{},any> {
+export class FetchData extends React.Component<{}, any> {
     constructor() {
         super();
         this.state = { forecasts: [], loading: true };
 
-        fetch('api/SampleData/WeatherForecasts')
-            .then(response => response.json() as Promise<WeatherForecast[]>)
+        fetch("api/SampleData/WeatherForecasts")
+            .then(response => response.json() as Promise<IWeatherForecast[]>)
             .then(data => {
                 this.setState({ forecasts: data, loading: false });
             });
     }
 
-    public render() {
-        let contents = this.state.loading
+    public render(): JSX.Element {
+        let contents: JSX.Element = this.state.loading
             ? <p><em>Loading...</em></p>
             : FetchData.renderForecastsTable(this.state.forecasts);
 
-        return <div>
-            <h1>Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            { contents }
-        </div>;
+        return  <div>
+                    <h1>Weather forecast</h1>
+                    <p>This component demonstrates fetching data from the server.</p>
+                    {contents}
+                </div>;
     }
 
-    private static renderForecastsTable(forecasts: WeatherForecast[]) {
-        return <table className='table'>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-            {forecasts.map(forecast =>
-                <tr key={ forecast.dateFormatted }>
-                    <td>{ forecast.dateFormatted }</td>
-                    <td>{ forecast.temperatureC }</td>
-                    <td>{ forecast.temperatureF }</td>
-                    <td>{ forecast.summary }</td>
-                </tr>
-            )}
-            </tbody>
-        </table>;
+    private static renderForecastsTable(forecasts: IWeatherForecast[]): JSX.Element {
+        return  <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Temp. (C)</th>
+                            <th>Temp. (F)</th>
+                            <th>Summary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {forecasts.map(forecast =>
+                            <tr key={forecast.dateFormatted}>
+                                <td>{forecast.dateFormatted}</td>
+                                <td>{forecast.temperatureC}</td>
+                                <td>{forecast.temperatureF}</td>
+                                <td>{forecast.summary}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>;
     }
 }
 
-interface WeatherForecast {
+interface IWeatherForecast {
     dateFormatted: string;
     temperatureC: number;
     temperatureF: number;
