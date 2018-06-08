8baf85b00c1d1d2733e46a99a04af44c81f329e0
斉藤育男 <ikuo@saitouikuonoMacBook-ea.local>
2018-06-08 23:16:55 +0900

    バグってたから直した

--- react/ClientApp/boot.tsx
+++ react/ClientApp/boot.tsx
@@ -6,7 +6,7 @@ import * as ReactDOM from "react-dom";
 import { Counter } from "../ClientApp/components/Counter";
 import { FetchData } from "../ClientApp/components/FetchData";
 function renderApp(): void {
-    ReactDOM.render(<Counter />,document.getElementById("counter1-app"));
+    ReactDOM.render(<Counter />,document.getElementById("counter-app"));
     ReactDOM.render(<FetchData />,document.getElementById("fetch-app"));
 }
 renderApp();
